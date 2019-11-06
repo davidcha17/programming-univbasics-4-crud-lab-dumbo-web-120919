9cd592c2f35ce4d3c14937d0e0193529fb60308d
David Cha <david.cha44@qmail.cuny.edu>
2019-11-06 17:48:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,22 +24,22 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  our_array = ["I", "am", "starting", "to"]
+  our_array = ["I", "am", "starting", "arrays!"]
   our_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  our_array = ["I", "am", "starting", "to"]
+  our_array = ["wow", "am", "starting", "to"]
   our_array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  our_array = ["I", "am", "starting", "to"]
+  our_array = ["wow", "am", "starting", "to"]
   our_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  our_array = ["I", "am", "starting", "to"]
+  our_array = ["I", "am", "starting", "arrays!"]
   our_array[-1]
 end
 
