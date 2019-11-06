17d86c27607e8020edb4657dbf371640cfaefd5d
David Cha <david.cha44@qmail.cuny.edu>
2019-11-06 17:34:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,39 @@
 def create_an_empty_array
-  
+  empty = []
 end
 
 def create_an_array
-  
+  tings = ["phone", "wallet", "laptop", "coffee"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "learning"]
+  element = ["Arrays!"]
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
